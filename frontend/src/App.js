cba83095bd998a276d0ae8bc7ce48fe50102a1d1
Meshach Adoe <madoe@ucsd.edu>
2021-02-20 22:37:41 +0700

    Add router and connect login/register to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './styles/styles.css'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Login from './components/Login'
 import Header from './components/Header'
 import RegisterApplicant from './components/RegisterApplicant'
@@ -7,10 +8,24 @@ import RegisterContributor from './components/RegisterContributor'
 function App() {
 	return (
 		<div className="App">
-			<Header />
-			{/* <Login /> */}
-			<RegisterApplicant />	
-			{/* <RegisterContributor />	 */}
+			<Router>
+				<Header />
+				<Switch>
+					<Route exact path="/">
+						<Login />
+					</Route>
+					<Route path="/register-applicant">
+						<RegisterApplicant />
+					</Route>
+					<Route path="/register-host">
+						<RegisterContributor />
+					</Route>
+					<Route path="/host">
+						<HostView />
+					</Route>
+				</Switch>
+			</Router>
+
 		</div>
 	);
 }
---
+++ frontend/src/components/HostView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HostView = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HostView
