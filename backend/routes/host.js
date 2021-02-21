059b4ddf996ec1c060ea75d51bd23a017889154f
Ly <kln014@ucsd.edu>
2021-02-20 19:55:33 -0800

    modified applicant routes

--- backend/routes/host.js
+++ backend/routes/host.js
@@ -2,9 +2,34 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth")
 const HostCard = require("../models/hostCard");
+const User = require("../models/user");
 
-router.post("/addCard", async (req,res) => {
+router.post("/addCard", auth, async (req,res) => {
+    // only add if user is authenticated
+    try {
+        const user = await User.findById(req.user.id);
+    } catch (err) {
+        console.log(err);
+        return res.status(401).json({ message: "Error in fetching user" });
+    }
 
+    try {
+        const { username, name, location, phone, maximumStay, description } = req.body;
+        let card = new HostCard({
+            username,
+            name,
+            location,
+            phone,
+            maximumStay,
+            description,
+        });
+
+        await card.save();
+        res.status(202).send(card);
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ message: "Error in saving new host card"});
+    }
 });
 
 router.get("/getAllCards", async (req, res) => {
